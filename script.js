bffb829ed5c2133386c3df69a2e0c47f7366c151
Nick <nickwoodward5@gmail.com>
2019-01-11 19:50:30 -0800

    Nearly done, need to fix navlinks

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
-var x = "y"
+$("#navIntroduction").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#Introduction").offset().top},
+        'slow');
+})
 
-console.log(x)
+$("#navWhatYouShouldKnow").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#What_you_should_already_know").offset().top},
+        'slow');
+})
+
+$("#navJavascriptAndJava").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#Javascript_and_Java").offset().top},
+        'slow');
+})
+
+$("#navHelloWorld").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#Hello_World").offset().top},
+        'slow');
+})
+
+$("#navVariables").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#Variables").offset().top},
+        'slow');
+})
+
+$("#navDeclaringVariables").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#Declaring_Variables").offset().top},
+        'slow');
+})
+
+$("#navDataTypes").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#Data_Types").offset().top},
+        'slow');
+})
